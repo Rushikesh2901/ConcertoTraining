8c1698b66129bd977e3011647d139c89b5de7b80
Rushikesh <mr.rushikeshrnk29@gmail.com>
2022-10-16 18:24:45 +0530

    commit

---
+++ SpringBootLoanProposalProject/src/main/java/com/dbs/web/mvc/HomeController.java
@@ -0,0 +1,114 @@
+package com.dbs.web.mvc;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.dbs.web.beans.Customer;
+import com.dbs.web.beans.User;
+import com.dbs.web.constants.LoanConstants;
+import com.dbs.web.service.CustomerService;
+
+
+
+@Controller
+
+public class HomeController {
+
+	@Autowired
+	private CustomerService service;
+	
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+	    binder.registerCustomEditor(String.class, new StringTrimmerEditor(true));
+	}
+	
+	@GetMapping("/register")
+	public String signup(Map<String, String[]> map,
+			Map<String, String> map1,
+			@RequestParam(required = false) String error)
+	{
+		map.put("types", LoanConstants.ID_TYPE);
+		map1.put("error",error);
+		return "register";
+	}
+	@GetMapping("/login")
+	public String login()
+	{
+		
+		return "login";
+	}
+	@GetMapping("/logout")
+	public String logout(HttpSession session)
+	{
+		session.removeAttribute("cust");
+		session.removeAttribute("custemail");
+		session.invalidate();
+		return "redirect:/login";
+	}
+	
+	void m1()
+
+	{
+		//page int x declares inside a method
+		//request = from 1 page to another within same req
+		// session => across request 
+		// application => static
+		//m2(x);
+	}
+	void m2()
+	{
+		
+	}
+	
+	@PostMapping("/login")
+	public String validate(User user,
+			HttpSession session,
+			Map<String , String> map)
+	{
+		
+		System.out.println("post user "+user);
+		Customer customer;
+		try {
+			customer = this.service.findCustomerByEmail(user.getEmail());
+			if(customer.getPassword().equals(user.getPassword())) {
+				session.setAttribute("custemail", user.getEmail());
+				session.setAttribute("cust", customer);
+				return "redirect:/customers/"+customer.getCustomerEmailId();
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			map.put("error", "Contact admin");
+		}
+		map.put("error", "Check your credentials");
+		return "login";
+	}
+	@PostMapping("/add")
+	public String insertCustomer(Customer customer, Map<String, String> map) {
+		System.out.println(customer);
+		String error = null;
+			try {
+				if(this.service.addCustomer(customer))
+				{
+					return "redirect:/login";
+				}
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				error = "Could not register, please try again later";
+				return "register";
+			}
+			error = "Could not register, email id already exists";
+			return "redirect:/register?error="+error;
+	}
+}
