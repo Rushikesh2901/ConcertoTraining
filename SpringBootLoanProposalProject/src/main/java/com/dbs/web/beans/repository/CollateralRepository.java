18a284e34580e9b637727980f7f5820ae0b1a818
Rushikesh2901 <72435456+Rushikesh2901@users.noreply.github.com>
2022-10-16 18:30:16 +0530

    Delete SpringBootLoanProposalProject directory

--- SpringBootLoanProposalProject/src/main/java/com/dbs/web/beans/repository/CollateralRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dbs.web.beans.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.dbs.web.beans.Collateral;
-
-public interface CollateralRepository  extends CrudRepository<Collateral, String>{
-
-}
